91deb49ac3a75de121b6ac16be1a829389d81604
joron1827 <61901261+joron1827@users.noreply.github.com>
2023-05-05 03:15:45 +0900

    modify code and update cronjob.yaml

--- crawler_using_db_hdfs/main.py
+++ crawler_using_db_hdfs/main.py
@@ -32,6 +32,8 @@ def main(argv, args) :
     print('\n')
     
     codes, page = dbHadoop.get_page_num(str(args.dbUser), str(args.dbPasswd),str(args.dbHost),str(args.dbName))
+
+    ## crawling 500 pages for each batch, batch start every 10 mins
     data, page, checkSum = crawler.ns_text_crawler(codes, page, term = 500)
     dbHadoop.update_page_num(codes, page, checkSum, str(args.dbUser), str(args.dbPasswd), str(args.dbHost),str(args.dbName))
     dbHadoop.save_hdfs(data, str(args.hdfs), int(args.hdfsPort), str(args.hdfs))
