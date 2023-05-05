290d7c71982744e7165e3d9419b98ca13c94ed9c
joron1827 <61901261+joron1827@users.noreply.github.com>
2023-05-06 00:15:50 +0900

    edit dbHadoop to hdfs

--- crawler_using_db_hdfs/dbHadoop.py
+++ crawler_using_db_hdfs/dbHadoop.py
@@ -6,7 +6,6 @@ def get_page_num(hdfs, port, path):
     import pyarrow.parquet as pq
     import pandas as pd
     
-    
     # Connect to HDFS
     hdfs = pa.HadoopFileSystem(host=hdfs, port=int(port))
     
@@ -29,30 +28,28 @@ def update_page_num(codes, page, checkSum, hdfs, port, path):
     import pyarrow.parquet as pq
     import pandas as pd
 
-    # Connect to HDFS
-    # HDFS 경로 설정
-    hdfs_path = f"{path}code/code_info.parquet"
-
-    # HDFS에 저장된 Parquet 파일 읽기
-    fs =  pa.HadoopFileSystem(host=hdfs, port=int(port))
-    table = pq.read_table(hdfs_path, filesystem=fs)
-
-    # PyArrow Table을 Pandas DataFrame으로 변환
-    df = table.to_pandas()
+    hdfs = pa.HadoopFileSystem(host=hdfs, port=int(port))
+    
+    file_name = "code/code_info.parquet"
+    hdfsPath = path + file_name
+    
+    with hdfs.open(hdfsPath, 'rb') as f:
+        table = pq.read_table(table, f)
+        df = table.to_pandas()
 
     if checkSum == False:
-        df.loc[df['stock_code'] == codes, 'page_num'] = page
+        df = df.loc[df['stock_code'] == codes, 'page_num'] = page
 
     if checkSum == True:
-        df.loc[df['stock_code'] == codes, 'check_num'] = 1
+        df = df.loc[df['stock_code'] == codes, 'check_num'] = 1
 
-    # 업데이트된 Pandas DataFrame을 PyArrow Table로 변환
     table = pa.Table.from_pandas(df)
-
-    # 업데이트된 PyArrow Table을 Parquet 파일로 저장
-    pq.write_table(table, hdfs_path, filesystem=fs)
     
-
+    with hdfs.open(hdfsPath, 'wb') as f:
+       pq.write_table(table, f)
+    
+    hdfs.close()
+    
     return 
 
 def save_hdfs(data, hdfs, port, path):
